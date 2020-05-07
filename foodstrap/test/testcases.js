a64c5eada957c2471137ef942f0433e87860c184
parvathysjsu <parvathy.kannankumarathmadomkrishnan@sjsu.edu>
2020-05-07 16:19:49 -0700

    changes to test mocha

--- foodstrap/test/testcases.js
+++ foodstrap/test/testcases.js
@@ -5,21 +5,13 @@ var cheerio = require('cheerio');
 describe('Automated test Cases', function () {
     describe('SignIn Page Test Cases', function () {
     it('SignIn page', function () {
-        request('http://localhost:5000/signin', function (error, response, body) {
-           // console.log(body);
-           expect(response.statusCode).to.equal(200);
-           // console.log(response.statusCode);                
+        request('http://localhost:5000/signin', function (error, response, body) {           
+            verifyPage(response);            
         });
     });
     it('SignIn page content', function () {
         request('http://localhost:5000/signin', function (error, response, body) {
-            var $ = cheerio.load(body);
-            var submit = $('#submit').attr('name');  
-            Promise.resolve(submit)
-            .then((submit) => {
-                //console.log(submit); 
-                expect(submit).to.equal('submit');
-            })                    
+            verifyPageContent(body);                              
         });
     });
     })
@@ -27,19 +19,12 @@ describe('Automated test Cases', function () {
     it('SignUp page', function () {
         request('http://localhost:5000/signup', function (error, response, body) {
            // console.log(body);
-           expect(response.statusCode).to.equal(200);
-           // console.log(response.statusCode);                
+           verifyPage(response);                        
         });
     });
     it('SignUp page content', function () {
         request('http://localhost:5000/signup', function (error, response, body) {
-            var $ = cheerio.load(body);
-            var submit = $('#submit').attr('name');  
-            Promise.resolve(submit)
-            .then((submit) => {
-                //console.log(submit); 
-                expect(submit).to.equal('submit');
-            })                    
+            verifyPageContent(body);                              
         });
     });
     })
@@ -47,19 +32,12 @@ describe('Automated test Cases', function () {
         it('shelters page', function () {
             request('http://localhost:5000/shelters', function (error, response, body) {
                // console.log(body);
-               expect(response.statusCode).to.equal(200);
-               // console.log(response.statusCode);                
+               verifyPage(response);                            
             });
         });
         it('shelters page content', function () {
             request('http://localhost:5000/shelters', function (error, response, body) {
-                var $ = cheerio.load(body);
-                var header = $('h1');
-                Promise.resolve(header)
-                .then((header) => {
-                  //  console.log(header); 
-                    expect(header).to.not.be.null;
-                })                    
+                verifyPageContentSheltersPage(body);                
             });
         });
         })
@@ -67,28 +45,47 @@ describe('Automated test Cases', function () {
     it('Setting page', function () {
         request('http://localhost:5000/setting', function (error, response, body) {
            // console.log(body);
-           expect(response.statusCode).to.equal(200);
-           // console.log(response.statusCode);                
+           verifyPage(response);                       
         });
     });
     it('Setting page content', function () {
         request('http://localhost:5000/setting', function (error, response, body) {
-            var $ = cheerio.load(body);
-            var submit = $('#submit').attr('name');  
-            Promise.resolve(submit)
-            .then((submit) => {
-                //console.log(submit); 
-                expect(submit).to.equal('submit');
-            })                    
+            verifyPageContent(body);           
         });
     });
 })
     describe('Negative flow Test Cases', function () {
     it('Wrong page', function () {
         request('http://localhost:5000/wrongpage', function (error, response, body) {
-            //console.log(body);            
-           expect(response.statusCode).to.equal(404);               
+            //console.log(body);   
+            verifyPageError(response);                              
         });
     });
 });
 });
+
+function verifyPageError(response) {
+    expect(response.statusCode).to.equal(404);    
+}
+function verifyPage(response) {
+    expect(response.statusCode).to.equal(200);    
+}
+function verifyPageContent(body) {
+    var $ = cheerio.load(body);
+    var submit = $('#submit').attr('name');  
+    Promise.resolve(submit)
+    .then((submit) => {
+        //console.log(submit); 
+        expect(submit).to.equal('submit');
+    })  
+}
+
+function verifyPageContentSheltersPage(body) {
+    var $ = cheerio.load(body);
+    var header = $('h1');
+    Promise.resolve(header)
+    .then((header) => {
+      //  console.log(header); 
+        expect(header).to.not.be.null;
+    })
+}
